f9fd417f4f11f983d9c237895fe112b3f1c295be
Youssef Elshabrawy <yielshabrawy@gmail.com>
2022-08-05 22:37:27 +0200

    working

--- src/App.js
+++ src/App.js
@@ -5,10 +5,17 @@ import Navbar from './components/Navbar';
 import Main from './components/Main';
 
 function App() {
+    const [darkM, setDarkM] = React.useState(false);
     return (
         <div className="container">
-            <Navbar />
-            <Main />
+            <Navbar
+                darkMode={darkM}
+                toggleDarkMode={() => {
+                    setDarkM((prev) => !prev);
+                    // console.log(darkM);
+                }}
+            />
+            <Main darkMode={darkM} />
         </div>
     );
 }
